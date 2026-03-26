b6f0ea605ba6c669adbcdc90f347a9d9ebd42841
gamalier007 <166254793+gamalier007@users.noreply.github.com>
2026-03-26 10:51:08 -0400

    Add files via upload

---
+++ Calificaciones01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Calificaciones01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("REPORTE DE CALIFICACIONES");
+            Console.WriteLine();
+
+            //Declaración de una lista
+            List<double> calificaciones = new List<double>();
+
+            //Agregar los elementos a la lista
+            calificaciones.Add(75);
+            calificaciones.Add(69);
+            calificaciones.Add(88);
+            calificaciones.Add(71);
+
+            foreach (double value in calificaciones) 
+            {
+                Console.WriteLine($"Calificación {value}");
+            }
+            double promedio= calificaciones.Average();
+            Console.WriteLine();
+
+            string mensaje = "El promedio es: ";
+            Console.WriteLine(mensaje + promedio);
+        }
+    }
+}
